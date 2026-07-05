584aef8fb3869b7973a13587567af7c25027a0b7
i.bogatskiy <i.bogatskiy@a1-systems.com>
2026-07-05 20:34:09 +0300

    Test via test container

--- src/test/java/com/ibdev/bot/zara/ZaraBotApplicationTests.java
+++ src/test/java/com/ibdev/bot/zara/ZaraBotApplicationTests.java
@@ -2,15 +2,23 @@ package com.ibdev.bot.zara;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 /**
  * @author i.bogatskii
  */
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(properties = "telegram.token=000:test-dummy")
 class ZaraBotApplicationTests {
+
+    @Container
+    @ServiceConnection
+    static final PostgreSQLContainer POSTGRES = new PostgreSQLContainer("postgres:17-alpine");
 
     @Test
     void contextLoads() {
     }
-
 }
